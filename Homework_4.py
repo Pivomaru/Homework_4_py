bce429c097cf4f9f0cefa3ec21ca7abb4ca9a7bb
Evgeniy <spardasenpai@gmail.com>
2022-09-19 19:46:04 +0300

    2nd zadacha

--- Homework_4.py
+++ Homework_4.py
@@ -1,14 +1,25 @@
 # Вычислить число pi c заданной точностью d Пример:
 # - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
 
-import math
+# import math
 
-pi = math.pi
-print('π = ', pi)
-d = 0.001
-print(f'точность = {d}')
-count = 0
-while d < 1:
-    count += 1
-    d = d*10
-print(round(pi, count))
+# pi = math.pi
+# print('π = ', pi)
+# d = 0.001
+# print(f'точность = {d}')
+# count = 0
+# while d < 1:
+#     count += 1
+#     d = d*10
+# print(round(pi, count))
+
+################################################################
+
+# Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+# a = int(input('Введите число: '))
+# list =[]
+# for i in range(1,a+1):
+#     if(a%i==0):
+#       list.append(i)
+# print(f'{a} = {list}')
